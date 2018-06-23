6054c5e1fcdc9bf5188ab72942415bb5aff46ad1
Gustavo Vieira <Grallyan SSD@DESKTOP-F5FLIN1>
2018-06-23 16:20:03 -0300

    Foi criado arquivos index para todos os diretório e foi movidos os arquivos da pasta cads para a pasta config.

--- site_cyber2pi_com_br/index.php
+++ site_cyber2pi_com_br/index.php
@@ -4,6 +4,15 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+include 'cads/index.php';
+include 'config/index.php';
+include 'imgs/index.php';
+include 'login/index.php';
+include 'logout/index.php';
+include 'logs/index.php';
+include 'sons/index.php';
+?>
 <html>
     <head>
         <meta charset="UTF-8">
