7be3cfc9fbb819e1eea72ddfcccb21ee9e665684
Gustavo Vieira <Grallyan SSD@DESKTOP-F5FLIN1>
2018-06-27 11:54:50 -0300

    Tentando ajustar css do site

--- site_cyber2pi_com_br/index.php
+++ site_cyber2pi_com_br/index.php
@@ -17,8 +17,52 @@ include 'sons/index.php';
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="css/index.css" rel="stylesheet" />
+        <link href="css/msgs_topo.css" rel="stylesheet" />
     </head>
     <body>
+        <div id="div_topo">
+            Cyber2pi
+        </div>
+        <div id="div_meio_rodape">
+            <div id="div_meio_conteudo">
+                <div id="div_meio_conteudo_dentro">
+                    <div class="div_meio_conteudo_dentro_float">
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                    </div>
+                    <div class="div_meio_conteudo_dentro_float">
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                    </div>
+                    <div class="div_meio_conteudo_dentro_float">
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                        Cyber2pi Cyber2pi Cyber2pi Cyber2pi
+                    </div>
+                    <div style="clear: left;"></div>
+                    <div class="div_meio_conteudo_dentro_float">
+                        Cyber2pi
+                    </div>
+                    <div class="div_meio_conteudo_dentro_float">
+                        Cyber2pi
+                    </div>
+                    <div class="div_meio_conteudo_dentro_float">
+                        Cyber2pi
+                    </div>
+                </div> 
+                <div id="div_rodape_conteudo">
+                    <div id="div_rodape_conteudo_dentro">
+                        Cyber2pi  
+                    </div>  
+                </div>
+            </div> 
+        </div>
         <?php
         // put your code here
         ?>
